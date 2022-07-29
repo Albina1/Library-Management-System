b8b014818b1bf2c46d18202b13bf09bbd8f3064e
Albina1 <pralbi1923@gmail.com>
2022-07-29 17:37:15 +0545

    Add files via upload

---
+++ Dblogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dblogin;
+
+/**
+ *
+ * @author Jagdish Duwal
+ */
+public class Dblogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginJFrame lFrame = new LoginJFrame();
+        lFrame.setVisible(true);
+        
+        
+         
+    }
+    
+}
---
+++ MySQLConnect.java
@@ -0,0 +1,30 @@
+package libmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ErSKS
+ */
+public class MySQLConnect {
+
+    public static Connection conn;
+
+    public static Connection connectDb() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/lib_db", "root", "");
+            System.out.println("Database Connected Successfully !");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Database cannot be connected !");
+            System.exit(0);
+        }
+        return null;
+    }
+
+}
